30ecb9a689ad761f34d15441c5a7220f9e24aeb9
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-10-23 00:08:50 -0500

    created new rails
    generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talky::Application.config.secret_key_base = '380677bb4817293e35f551cd14cadfce2ee401b0f6ce9878c22304c058b3834de350c67d95373d160ebec0d61879f8cd2dbb7708b3d10a7d82f41b37159eb636'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Talky::Application.config.session_store :cookie_store, key: '_talky_session'
